0fa0fa2b5c5b44edf830805ce07fd4910e9241d0
Harry Vu <harry@moonivia.com>
2023-08-09 03:59:19 -0400

    Clean project

---
+++ src/IllegalOperationException.java
@@ -0,0 +1,7 @@
+ public class IllegalOperationException extends Exception
+ {
+     public IllegalOperationException()
+        {
+            super("Error: Invalid operation. Please enter a valid operation!");
+        }
+ }
---
+++ src/InvalidOperandException.java
@@ -0,0 +1,6 @@
+public class InvalidOperandException extends Exception {
+    public InvalidOperandException()
+        {
+            super("Error: Invalid operand. Please enter a valid operand!");
+        }
+}
